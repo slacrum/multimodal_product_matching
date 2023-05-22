d163f8c6d2e20339b55042eb5103ddec3f052c82
Simulacrum <39220667+SimuIacrum@users.noreply.github.com>
2023-05-22 23:44:09 +0200

    updated MNN-EM experiments up to training added the following: * Image Processing (loading img model + creating img embeddings) * Saving img+txt embeddings * Train-Test-Split * Callbacks (Earlystopping, Checkpoint, Tensorboard) * Actual Training

--- models/mnn_em.py
+++ models/mnn_em.py
@@ -1,11 +1,12 @@
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input, Dense, Multiply, BatchNormalization
 from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.metrics import Recall, Precision, BinaryAccuracy, CosineSimilarity
 from models.char_cnn_zhang import CharCNNZhang
 
 
 class MNNEM(object):
-    def __init__(self, img_input_size, img_conv_layers, txt_input_size, txt_conv_layers, txt_weights, char_cnn_config, combined_conv_layers, learning_rate, loss='binary_crossentropy', metrics=["recall", "precision", "binary_accuracy", "cosine_similarity"]) -> None:
+    def __init__(self, img_input_size, img_conv_layers, txt_input_size, txt_conv_layers, txt_weights, char_cnn_config, combined_conv_layers, learning_rate, loss='binary_crossentropy') -> None:
         self.img_input_size = img_input_size
         self.img_conv_layers = img_conv_layers
         self.txt_input_size = txt_input_size
@@ -15,9 +16,7 @@ class MNNEM(object):
         self.combined_conv_layers = combined_conv_layers
         self.learning_rate = learning_rate
         self.loss = loss
-        self.metrics = metrics
         self._build_model()  # builds self.model variable
-
 
     def _build_model(self):
         # Input Layer
@@ -26,7 +25,7 @@ class MNNEM(object):
         # fc + ReLU
         for i, img_cl in enumerate(self.img_conv_layers[:-1], 1):
             x = Dense(img_cl, activation='relu',
-                                name=f"Image_FC_{i}")(img_features if i == 1 else x)
+                      name=f"Image_FC_{i}")(img_features if i == 1 else x)
 
         # fc + L2 Norm
         x = Dense(
@@ -38,13 +37,13 @@ class MNNEM(object):
         text_features = Input(shape=(self.txt_input_size), name="Text_Input")
 
         x = CharCNNZhang(**self.char_cnn_config)
-        
+
         x = x.model(text_features)
-                
+
         # fc + ReLU
         for i, txt_cl in enumerate(self.txt_conv_layers[:-1], 1):
             x = Dense(txt_cl, activation='relu',
-                                name=f"Text_FC_{i}")(x)
+                      name=f"Text_FC_{i}")(x)
 
         # fc + L2 Norm
         x = Dense(
@@ -58,14 +57,22 @@ class MNNEM(object):
         # FC Layers
         for i, comb_cl in enumerate(self.combined_conv_layers, 1):
             combined = Dense(comb_cl, activation='relu',
-                            name=f"Combined_FC_{i}")(combined)
+                             name=f"Combined_FC_{i}")(combined)
 
         output = Dense(1, activation='sigmoid', name="Sigmoid")(combined)
         model = Model(inputs=[img_features, text_features], outputs=output)
 
         optimizer = Adam(learning_rate=self.learning_rate)
 
-        model.compile(optimizer=optimizer, loss=self.loss, metrics=self.metrics)
+        metrics = [
+        Recall(thresholds=0.5, top_k=1, class_id=None, name="recall", dtype=None),
+        Precision(thresholds=0.5, top_k=1, class_id=None, name="precision", dtype=None),
+        BinaryAccuracy(name="binary_accuracy", dtype=None, threshold=0.5),
+        CosineSimilarity(name='cosine_similarity', dtype=None, axis=-1)
+        ]
+
+        model.compile(optimizer=optimizer, loss=self.loss,
+                      metrics=metrics)
         self.model = model
         # print("MNN-EM model built: ")
         # self.model.summary()
---
+++ utils/img_models.py
@@ -0,0 +1,35 @@
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.applications import MobileNetV3Large, MobileNetV3Small
+from tensorflow.keras.applications.resnet import ResNet152
+from tensorflow.keras.applications.vgg19 import VGG19
+
+def load(name):
+    model_dict = {
+        "MobilenetV3large": MobileNetV3Large,
+        "MobilenetV3small": MobileNetV3Small,
+        "resnet152": ResNet152,
+        "VGG19": VGG19
+    }
+    if name in model_dict:
+        img_model = model_dict[name](
+            include_top=False, weights='imagenet', input_tensor=None,
+            input_shape=None, pooling="max", classes=None
+        )
+        return img_model
+    else:
+        raise ValueError("Invalid model name: {}".format(name))
+
+
+def create_embeddings_from(img_model, img, path):
+    datagenerator = ImageDataGenerator()
+
+    img = datagenerator.flow_from_dataframe(
+        dataframe=img, directory=path, x_col='path', y_col=None,
+        weight_col=None, target_size=(256, 256), color_mode='rgb',
+        classes=None, class_mode="input", batch_size=1, shuffle=False,
+        seed=None, save_to_dir=None, save_prefix='',
+        save_format='jpg', subset=None, interpolation='nearest',
+        validate_filenames=True
+        )
+
+    return img_model.predict(img)
