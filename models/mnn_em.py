7da461cbb160273c26baedbe87e968e027ea8644
Simulacrum <39220667+SimuIacrum@users.noreply.github.com>
2023-05-25 17:05:03 +0000

    separate MNN-EM model into "head" and "tail" part and also rerun experiments to reflect changes. "head" includes all layers up to element-wise multiplication, while "tail" includes the rest. In particular, introducing "head" component will facilitate implementation of extended MNN-EM later on

--- models/mnn_em.py
+++ models/mnn_em.py
@@ -6,12 +6,8 @@ from models.char_cnn_zhang import CharCNNZhang
 
 
 class MNNEM(object):
-    def __init__(self, img_input_size, txt_input_size, txt_weights, char_cnn_config, fc_layers, combined_fc_layers, learning_rate, metrics=["recall", "precision", "binary_accuracy", "cosine_similarity"], loss='binary_crossentropy') -> None:
-        self.img_input_size = img_input_size
-        self.txt_input_size = txt_input_size
-        self.txt_weights = txt_weights
-        self.char_cnn_config = char_cnn_config
-        self.fc_layers = fc_layers
+    def __init__(self, head_config, combined_fc_layers, learning_rate, metrics=["recall", "precision", "binary_accuracy", "cosine_similarity"], loss='binary_crossentropy') -> None:
+        self.head_config = head_config
         self.combined_fc_layers = combined_fc_layers
         self.learning_rate = learning_rate
         self.metrics = metrics
@@ -19,47 +15,58 @@ class MNNEM(object):
         self._build_model()  # builds self.model variable
 
     def _build_model(self):
+        # # Input Layer
+        # img_features = Input(shape=(self.img_input_size), name="Image_Input")
+
+        # # fc + ReLU
+        # for i, fl in enumerate(self.img_fc_layers[:-1], 1):
+        #     x = Dense(fl, activation='relu',
+        #               name=f"Image_FC_{i}")(img_features if i == 1 else x)
+
+        # # fc + L2 Norm
+        # x = Dense(
+        #     self.img_fc_layers[-1], kernel_regularizer='l2', name="Image_FC_last")(img_features if len(self.img_fc_layers) == 1 else x)
+
+        # output_img = BatchNormalization(name="Image_Batch_Normalization")(x)
+
+        # # Input Layer
+        # text_features = Input(shape=(self.txt_input_size), name="Text_Input")
+
+        # x = CharCNNZhang(**self.char_cnn_config)
+
+        # x = x.model(text_features)
+
+        # # fc + ReLU
+        # for i, fl in enumerate(self.txt_fc_layers[:-1], 1):
+        #     x = Dense(fl, activation='relu',
+        #               name=f"Text_FC_{i}")(x)
+
+        # # fc + L2 Norm
+        # x = Dense(
+        #     self.txt_fc_layers[-1], kernel_regularizer='l2', name="Text_FC_last")(text_features if len(self.txt_fc_layers) == 1 else x)
+
+        # output_text = BatchNormalization(name="Text_Batch_Normalization")(x)
+
+        # # Element-wise product
+        # combined = Multiply(
+        #     name="Element-wise_Multiplication")([output_img, output_text])
+
         # Input Layer
-        img_features = Input(shape=(self.img_input_size), name="Image_Input")
-
-        # fc + ReLU
-        for i, fl in enumerate(self.fc_layers[:-1], 1):
-            x = Dense(fl, activation='relu',
-                      name=f"Image_FC_{i}")(img_features if i == 1 else x)
-
-        # fc + L2 Norm
-        x = Dense(
-            self.fc_layers[-1], kernel_regularizer='l2', name="Image_FC_last")(x)
-
-        output_img = BatchNormalization(name="Image_Batch_Normalization")(x)
+        img_features = Input(shape=(self.head_config["img_input_size"]), name="Image_Input")
 
         # Input Layer
-        text_features = Input(shape=(self.txt_input_size), name="Text_Input")
+        text_features = Input(shape=(self.head_config["txt_input_size"]), name="Text_Input")
 
-        x = CharCNNZhang(**self.char_cnn_config)
+        x = MNNEMHead(**self.head_config)
 
-        x = x.model(text_features)
+        x = x.model([img_features, text_features])
 
-        # fc + ReLU
-        for i, fl in enumerate(self.fc_layers[:-1], 1):
-            x = Dense(fl, activation='relu',
-                      name=f"Text_FC_{i}")(x)
-
-        # fc + L2 Norm
-        x = Dense(
-            self.fc_layers[-1], kernel_regularizer='l2', name="Text_FC_last")(x)
-
-        output_text = BatchNormalization(name="Text_Batch_Normalization")(x)
-
-        # Element-wise product
-        combined = Multiply(
-            name="Element-wise_Multiplication")([output_img, output_text])
         # FC Layers
         for i, comb_fl in enumerate(self.combined_fc_layers, 1):
-            combined = Dense(comb_fl, activation='relu',
-                             name=f"Combined_FC_{i}")(combined)
+            x = Dense(comb_fl, activation='relu',
+                             name=f"Combined_FC_{i}")(x)
 
-        output = Dense(1, activation='sigmoid', name="Sigmoid")(combined)
+        output = Dense(1, activation='sigmoid', name="Sigmoid")(x)
         model = Model(inputs=[img_features, text_features], outputs=output)
 
         optimizer = Adam(learning_rate=self.learning_rate)
@@ -69,3 +76,54 @@ class MNNEM(object):
         self.model = model
         # print("MNN-EM model built: ")
         # self.model.summary()
+
+class MNNEMHead(object):
+    def __init__(self, img_input_size, txt_input_size, txt_weights, char_cnn_config, img_fc_layers, txt_fc_layers):
+        self.img_input_size = img_input_size
+        self.txt_input_size = txt_input_size
+        self.txt_weights = txt_weights
+        self.char_cnn_config = char_cnn_config
+        self.img_fc_layers = img_fc_layers
+        self.txt_fc_layers = txt_fc_layers
+        self._build_model()  # builds self.model variable
+
+    def _build_model(self):
+        # Input Layer
+        img_features = Input(shape=(self.img_input_size))
+
+        # fc + ReLU
+        for i, fl in enumerate(self.img_fc_layers[:-1], 1):
+            x = Dense(fl, activation='relu',
+                      name=f"Image_FC_{i}")(img_features if i == 1 else x)
+
+        # fc + L2 Norm
+        x = Dense(
+            self.img_fc_layers[-1], kernel_regularizer='l2', name="Image_FC_last")(img_features if len(self.img_fc_layers) == 1 else x)
+
+        output_img = BatchNormalization(name="Image_Batch_Normalization")(x)
+
+        # Input Layer
+        text_features = Input(shape=(self.txt_input_size))
+
+        x = CharCNNZhang(**self.char_cnn_config)
+
+        x = x.model(text_features)
+
+        # fc + ReLU
+        for i, fl in enumerate(self.txt_fc_layers[:-1], 1):
+            x = Dense(fl, activation='relu',
+                      name=f"Text_FC_{i}")(x)
+
+        # fc + L2 Norm
+        x = Dense(
+            self.txt_fc_layers[-1], kernel_regularizer='l2', name="Text_FC_last")(text_features if len(self.txt_fc_layers) == 1 else x)
+
+        output_text = BatchNormalization(name="Text_Batch_Normalization")(x)
+
+        # Element-wise product
+        combined = Multiply(
+            name="Element-wise_Multiplication")([output_img, output_text])
+        
+        model = Model(inputs=[img_features, text_features], outputs=combined, name="MNN_EM_Head")
+
+        self.model = model
